c0aa3ec886e1f9237d830d9e2425d2f0bc5b0c81
FelicityWY <1147102425@qq.com>
2019-10-30 14:56:48 +0800

    Update gd_dash_line.c

--- tests/gd/gd_dash_line.c
+++ tests/gd/gd_dash_line.c
@@ -5,10 +5,9 @@ int main()
 
 	im = gdImageCreate(64,64);
 	int white;
-	int black;
-	black = gdImageColorAllocate(im, 0, 0, 0);
+	gdImageColorAllocate(im, 0, 0, 0);
 	white = gdImageColorAllocate(im, 255, 255, 255);
 
 	gdImageDashedLine(im, 0, 0, 63, 63, white);
-	return gdNumFailures();
+	return 1;
 }
