b0bd6b2d41c79c9484b47fe4e4bc024bbb5d2ca4
willson-chen <53104026+willson-chen@users.noreply.github.com>
2019-10-31 15:32:05 +0800

    Update gd_dash_line.c

--- tests/gd/gd_dash_line.c
+++ tests/gd/gd_dash_line.c
@@ -10,11 +10,11 @@ int main()
 	gdImageColorAllocate(im, 0, 0, 0);
 	white = gdImageColorAllocate(im, 255, 255, 255);
 	gdImageDashedLine(im, 0, 0, 64, 64, white);
-	
-//	if(gdAssertImageEqualsToFile("gd/test.png",im))
-//	{
-//		return 1;
-//	}
+
+	if(gdAssertImageEqualsToFile("gd/test.png",im))
+	{
+		return 1;
+	}
 	gdImageDestroy(im);
 	return 0;
 }
