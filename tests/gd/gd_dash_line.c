8a94a12542ca36a6293df6123b96fdaf5f0c3665
FelicityWY <1147102425@qq.com>
2019-10-31 16:02:21 +0800

    Update gd_dash_line.c

--- tests/gd/gd_dash_line.c
+++ tests/gd/gd_dash_line.c
@@ -1,17 +1,16 @@
 #include "gd.h"
+#include "gdtest.h"
 
 int main()
 {
 	gdImagePtr im;
-	im = gdImageCreate(64,64);
+	im = gdImageCreate(64, 64);
 	int white;
-	FILE *pngout;
+
 	gdImageColorAllocate(im, 0, 0, 0);
 	white = gdImageColorAllocate(im, 255, 255, 255);
-	gdImageDashedLine(im, 0, 0, 63, 63, white);
-	pngout = fopen("test.png", "wb");
-	gdImagePng(im, pngout);
-	fclose(pngout);
+	gdImageDashedLine(im, 0, 0, 64, 64, white);
+
 	gdImageDestroy(im);
 	return 0;
 }
