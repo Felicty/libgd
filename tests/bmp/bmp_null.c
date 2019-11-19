0801bba5f3c4b458d2276e76e59fbcad9a400210
FelicityWY <1147102425@qq.com>
2019-11-19 15:49:39 +0800

    Update bmp_null.c

--- tests/bmp/bmp_null.c
+++ tests/bmp/bmp_null.c
@@ -3,12 +3,15 @@
 int main()
 {
 	gdImagePtr im;
-
+	double m[6];
+	float angle = 180.0;
+	
 	im = gdImageCreateFromBmp(NULL);
 	if (im != NULL) {
 		gdImageDestroy(im);
 		return 1;
 	}
+	gdAffineShearHorizontal(m, angle);
 	gdImageBmp(im, NULL, 0); /* noop safely */
 	return 0;
 }
