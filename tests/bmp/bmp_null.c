7ab3b32ce69fb13ba3392387db76243fb4f9e24b
FelicityWY <1147102425@qq.com>
2019-11-19 17:00:07 +0800

    Update bmp_null.c

--- tests/bmp/bmp_null.c
+++ tests/bmp/bmp_null.c
@@ -1,17 +1,19 @@
 #include "gd.h"
 int main()
 {	
-	gdImagePtr im;
-
-	double m[6];
-	float angle = 180.0;
-
+	gdImagePtr im, imc;
+	FILE *inFile;
+	inFile = gdTestFileOpen2("bmp", "bug00450.bmp");
+	
 	im = gdImageCreateFromBmp(NULL);
+	imc = gdImageCreateFromBmp(inFile);
 	if (im != NULL) {
 		gdImageDestroy(im);
 		return 1;
 	}
 	gdAffineShearHorizontal(m, angle);
 	gdImageBmp(im, NULL, 0); /* noop safely */
+	
+	gdImageDestroy(imc);
 	return 0;
 }
